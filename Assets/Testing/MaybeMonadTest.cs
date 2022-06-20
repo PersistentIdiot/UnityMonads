7f19c6642e344653a2cce4b6542eb79b3241ab49
JonBrant <jonbrant@gmail.com>
2022-06-19 20:02:14 -0400

    Cleanup

--- Assets/Testing/MaybeMonadTest.cs
+++ Assets/Testing/MaybeMonadTest.cs
@@ -5,39 +5,28 @@ using Com.PI.UnityMonads.Examples;
 using PI.Monads.Unity;
 using UnityEngine;
 
+
 public class MaybeMonadTest : MonoBehaviour {
     [SerializeField] private GameObject TestObject;
-   // private GameObjectMaybeDefaultRepo repo;
     private void Awake() {
-        
+
         // Bug: This should require EnsureGameObjectNotNull to be passed to Bind, but appears to work without it. Adding bind causes multiple new objects to be created.
         // Try moving test data into repo instead.
         // New Bug: New object is created in both cases.
-        
+
         GameObjectMaybeDefaultRepo repo = new GameObjectMaybeDefaultRepo();
         repo.InitTestData();
         Maybe<GameObject, string> GameObjectOrError = repo.MaybeGetNewGameObject("Default").Bind(TestObject, EnsureGameObjectNotNull) as Maybe<GameObject, string>;
         Debug.LogFormat($"{GameObjectOrError.Value.name}");
     }
 
-    private Monad<GameObject, string> EnsureGameObjectNotNull(GameObject arg) { if (arg == default) {
-            return new StringToGameObjectOrException(new GameObject("New Object."));
-            //throw new Exception($"{nameof(Client)}.{nameof(EnsureGameObjectNotNull)}() - {nameof(arg)} was null. ");
-        }
-        else {
-            return new Maybe<GameObject, string>(arg);
-        } }
-
-    /*
     private Monad<GameObject, string> EnsureGameObjectNotNull(GameObject arg) {
         if (arg == default) {
             return new StringToGameObjectOrException(new GameObject("New Object."));
-            //throw new Exception($"{nameof(Client)}.{nameof(EnsureGameObjectNotNull)}() - {nameof(arg)} was null. ");
+            //throw new Exception($"{nameof(Client)}.{nameof(EnsureGameObjectNotNull)}() - {nameof(arg)} was default. ");
         }
         else {
             return new Maybe<GameObject, string>(arg);
         }
     }
-    */
-
 }
